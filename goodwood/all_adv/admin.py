e552d32188317be971244ea8570066987562f07a
Max Zanchenko <zamax77@mail.ru>
2023-02-28 16:20:30 +0300

    добавлены категории

--- goodwood/all_adv/admin.py
+++ goodwood/all_adv/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from all_adv.models import Adv
+from all_adv.models import Adv, Rubric
 
-admin.site.register(Adv)
+
+class AdvAdmin(admin.ModelAdmin):
+    list_display = ('title','rubric','content','price','published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title','price','rubric')
+class RubricAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_display_links = ('name',)
+    search_fields = ('name',)
+
+admin.site.register(Adv,AdvAdmin)
+admin.site.register(Rubric,RubricAdmin)
--- goodwood/all_adv/models.py
+++ goodwood/all_adv/models.py
@@ -8,11 +8,25 @@ class Adv(models.Model):
     #Обратите внимание, что это отличается от null. null связана исключительно с базой данных, тогда как blank связана с проверкой. Если
     # поле имеет blank = True, проверка формы позволит ввести пустое значение. Если поле имеет blank = False, поле будет обязательным.
     price = models.FloatField(null=True,blank =True,verbose_name='Цена')
-
+    rubric = models.ForeignKey('Rubric',null=True,on_delete=models.PROTECT, verbose_name='Рубрика')
     published = models.DateTimeField(auto_now_add=True, db_index=True,verbose_name='Опубликовано от')
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name_plural='Объявления'
         verbose_name = 'Объявление'
         ordering = ['-published']
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20,db_index=True,verbose_name='Название')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+
+
 
