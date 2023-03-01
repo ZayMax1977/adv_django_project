4d1966a70eb5e8b8304268189c74d306f238c32d
Max Zanchenko <zamax77@mail.ru>
2023-03-01 16:14:05 +0300

    именованные параметры в urls

--- goodwood/all_adv/urls.py
+++ goodwood/all_adv/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from all_adv.views import index
+from all_adv.views import index, by_rubric
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric')
 
 ]
--- goodwood/all_adv/views.py
+++ goodwood/all_adv/views.py
@@ -1,12 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from all_adv.models import Adv
+from all_adv.models import Adv, Rubric
 
 
 def index(requests):
     advs = Adv.objects.all()
+    rubrics = Rubric.objects.all()
+    context ={
+        "advs": advs,
+        "rubrics":rubrics
+    }
+    return render(requests,'all_adv/index.html',    context)
+
+def by_rubric(requests,rubric_id):
+    advs_by_rub = Adv.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {
+        'advs_by_rub':advs_by_rub,
+        'rubrics': rubrics,
+        'current_rubric': current_rubric
+    }
+    return render(requests,'all_adv/by_rubric.html',context)
 
 
-    return render(requests,'all_adv/index.html',{"advs":advs})
+
+
 
